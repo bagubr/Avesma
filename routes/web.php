6674d7774dc058bf194fdd7ee64e4ccbc24f9b9d
misft <indrawandwiprasetyo@gmail.com>
2021-10-30 14:08:55 +0700

    [init]

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Octane\Facades\Octane;
+use Symfony\Component\HttpFoundation\Response;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/test", function() use (&$values) {
+    return "Hello world";    
+});
+
+
