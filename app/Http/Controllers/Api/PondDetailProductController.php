8d3c5fa63f764890dbb50835088fc5dc102c118a
SATRIO <satriotol69@gmail.com>
2021-11-18 14:31:40 +0700

    fixing validasi

--- app/Http/Controllers/Api/PondDetailProductController.php
+++ app/Http/Controllers/Api/PondDetailProductController.php
@@ -3,26 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PondDetailProduct\CreatePondDetailProductRequest;
 use App\Models\PondDetailProduct;
 use App\Repositories\PondDetailProductRepository;
 use Illuminate\Http\Request;
 
 class PondDetailProductController extends Controller
 {
-    public function index(Request $request) {
+    public function index(Request $request)
+    {
         return $this->sendSuccessResponse([
-            'products'=>PondDetailProductRepository::get($request->toArray())
+            'products' => PondDetailProductRepository::get($request->toArray())
         ]);
     }
 
-    public function store(Request $request) {
-        $pond_detail_product = PondDetailProduct::create([
-            'pond_detail_id'=>$request->pond_detail_id,
-            'name'=>$request->name
-        ]);
-
+    public function store(CreatePondDetailProductRequest $request)
+    {
+        $data = $request->all();
+        $pond_detail_product = PondDetailProduct::create($data);
         return $this->sendSuccessResponse([
-            'product'=>$pond_detail_product
+            'product' => $pond_detail_product
         ]);
     }
 }
