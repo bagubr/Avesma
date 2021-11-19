e6c477426ca444664a0684704a2deb8676edc498
SATRIO <satriotol69@gmail.com>
2021-11-19 16:24:55 +0700

    income relation

--- app/Models/Income.php
+++ app/Models/Income.php
@@ -12,18 +12,12 @@ class Income extends Model
     protected $fillable = [
         'pond_detail_id',
         'reported_at',
-        'pond_detail_product_id'
     ];
     public function pond_detail()
     {
         return $this->belongsTo(PondDetail::class, 'pond_detail_id');
     }
-
-    public function pond_detail_product()
-    {
-        return $this->belongsTo(PondDetailProduct::class);
-    }
-
+    
     public function income_detail()
     {
         return $this->hasMany(IncomeDetail::class, 'income_id', 'id');
