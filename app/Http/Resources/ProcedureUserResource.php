120b174ed87f75bc4c814ca96c636c8f6a8aaa83
SATRIO <satriotol69@gmail.com>
2021-11-22 16:32:36 +0700

    fixing procedure user resource

--- app/Http/Resources/ProcedureUserResource.php
+++ app/Http/Resources/ProcedureUserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\FormProcedureFormula;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProcedureUserResource extends JsonResource
@@ -19,9 +20,19 @@ class ProcedureUserResource extends JsonResource
             "user_id" =>  $this->user_id,
             "user_name" =>  $this->user?->name,
             "pond_detail_id" => $this->pond_detail_id,
+            "form_procedure_id" => $this->form_procedure_id,
+            "form_procedure_name" => $this->form_procedure?->fish_and_procedure,
             "total_score" => $this->form_procedure_detail_input->sum('score'),
+            "parameter" => $this->getFormula($this->form_procedure_id, $this->form_procedure_detail_input->sum('score'))->note,
             "reported_at" => $this->reported_at,
             "procedure_details" => ProcedureDetailResource::collection($this->form_procedure_detail_input)
         ];
+    }
+
+    public function getFormula($id, $score)
+    {
+        $formula = FormProcedureFormula::where('form_procedure_id', $id)->where('min_range', '<=', $score)
+            ->where('max_range', '>=', $score)->first();
+        return $formula;
     }
 }
