052a3d3e1b089bbfa6e2f00c44ccbc56d1aac11f
SATRIO <satriotol69@gmail.com>
2021-11-23 16:42:31 +0700

    fish price fixing

--- app/Http/Controllers/Api/FishPriceController.php
+++ app/Http/Controllers/Api/FishPriceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\FishPriceResource;
 use App\Models\FishPrice;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,7 @@ class FishPriceController extends Controller
             $q->where($request->fish_species_id, 'fish_species_id');
         })->get();
         return $this->sendSuccessResponse([
-            'fish_prices' => $fish_prices,
+            'fish_prices' => FishPriceResource::collection($fish_prices),
         ]);
     }
     public function show(FishPrice $fish_price)
