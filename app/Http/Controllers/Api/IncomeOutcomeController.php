24c97bc5f1cb655a623db1eac40ba17f06d754b7
SATRIO <satriotol69@gmail.com>
2021-11-25 13:20:21 +0700

    income outcome controller done

--- app/Http/Controllers/Api/IncomeOutcomeController.php
+++ app/Http/Controllers/Api/IncomeOutcomeController.php
@@ -3,14 +3,47 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Income;
+use App\Models\IncomeDetail;
+use App\Models\Outcome;
+use App\Models\OutcomeDetail;
 use Illuminate\Http\Request;
 
 class IncomeOutcomeController extends Controller
 {
     public function index(Request $request)
     {
-        $incomes = Income::where('pond_detail_id', $request->pond_detail_id)->get();
-        dd($incomes);
+        $income_total = IncomeDetail::whereHas('income', function ($q) use ($request) {
+            $q->where('pond_detail_id', $request->pond_detail_id);
+        })->sum('total_price');
+        $outcome_tetap = Outcome::where('pond_detail_id', $request->pond_detail_id)
+            ->whereHas('outcome_detail.outcome_setting', function ($sq) {
+                $sq->where('outcome_category_id', 1);
+            })->orderBy('id', 'desc');
+        $outcomes_lain = Outcome::where('pond_detail_id', $request->pond_detail_id)
+            ->orderBy('reported_at', 'desc')->whereHas('outcome_detail.outcome_setting', function ($sq) {
+                $sq->where('outcome_category_id', 2);
+            })->get();
+        $outcome_total = $outcome_tetap->first()->total_nominal ?? 0 + $outcomes_lain->sum('total_nominal') ?? 0;
+        $calculation = $income_total / $outcome_total;
+
+        if ($calculation > 1) {
+            $calculation_message = "Diatas";
+            $calculation_status = ">";
+        } else if ($calculation < 1) {
+            $calculation_message = "Dibawah";
+            $calculation_status = "<";
+        } else {
+            $calculation_message = "Sama";
+            $calculation_status = "=";
+        }
+        return $this->sendSuccessResponse([
+            'income_total' => $income_total,
+            'outcome_total' => $outcome_total,
+            'calculation' => [
+                'calculation_total' => $calculation,
+                'calculation_status' => $calculation_status,
+                'calculation_message' => $calculation_message,
+            ],
+        ]);
     }
 }
