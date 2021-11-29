192c6453fdaf10d8c5dd8981ee80a8e1cdfb6e65
Bagu bragowo <bagubragowo3@gmail.com>
2021-11-29 16:03:59 +0700

    share deeplink

--- app/Http/Controllers/Api/ShareController.php
+++ app/Http/Controllers/Api/ShareController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\ArticleProcedure;
+use App\Models\ArticleRecipe;
 use Illuminate\Http\Request;
 
 class ShareController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('share');
+        $site = env('APP_NAME');
+        $article = Article::find($request->get('id'));
+        $url = 'intent://view?id=123#Intent;package=com.cancreative.avesma;scheme=http;launchFlags=268435456;end;';
+        return view('share', compact('url', 'article', 'site'));
     }
 }
