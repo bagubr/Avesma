d213ac599d2bf375aa396e0e66a7df6d63350133
Bagu bragowo <bagubragowo3@gmail.com>
2021-11-29 20:00:54 +0700

    route api notification

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Admin\Actions\User\Restore;
 use App\Models\User;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
@@ -11,6 +12,7 @@ use App\Admin\Actions\User\StarUser;
 use App\Models\Region;
 use App\Models\UserInformation;
 use Encore\Admin\Grid\Displayers\Actions;
+use Illuminate\Http\Request;
 
 class UserController extends AdminController
 {
@@ -35,7 +37,7 @@ class UserController extends AdminController
             $filter->like('name', 'Name');
             $filter->like('email', 'Email');
             $filter->like('phone', 'Phone');
-
+            $filter->scope('trashed', 'Recycle Bin')->onlyTrashed();
         });
         $grid->quickSearch('name', 'email', 'phone');
         $grid->column('name', __('Name'));
@@ -48,6 +50,11 @@ class UserController extends AdminController
         $grid->column('created_at', __('Member Since'));
         $grid->column('updated_at', __('Updated at'));
         $grid->disableExport();
+        if ($request->get('_scope_') == 'trashed'){
+        $grid->actions(function ($actions){ 
+                $actions->add(new Restore());
+            });
+        }
         
         return $grid;
     }
@@ -107,7 +114,7 @@ class UserController extends AdminController
         $form = new Form(new User());
         $form->text('name', __('Name'))->required();
         $form->email('email', __('Email'))->required();
-        $form->mobile('phone', __('Phone'))->options(['mask' => '999999999999'])->required();
+        $form->mobile('phone', __('Phone'))->options(['mask' => '9999999999999'])->required()->value($form->model()->phone);
         $form->select('gender', __('Jenis Kelamin'))->options(['', 'Pria' => 'Pria', 'Wanita' => 'Wanita'])->required();
         $form->date('birth_date', __('Ulang Tahun'))->default(date('Y-m-d'))->required();
         $form->textarea('address', __('Address'))->required();
@@ -118,6 +125,10 @@ class UserController extends AdminController
         $form->disableCreatingCheck();
         $form->disableEditingCheck();
         $form->disableViewCheck();
+         $form->saving(function (Form $form)
+        {
+            $form->phone = '+'.preg_replace('/\D/', '', $form->phone);
+        });
         // $form->tab('User', function ($form) {
 
         //     $form->text('name', __('Name'))->required();
