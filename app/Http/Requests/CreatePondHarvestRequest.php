eb255cb051c87cde73d4e148c6d161f023490405
SATRIO <satriotol69@gmail.com>
2021-11-30 11:31:47 +0700

    create pond harvest

---
+++ app/Http/Requests/CreatePondHarvestRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Requests;
+
+
+class CreatePondHarvestRequest extends ApiRequest
+{
+    public function rules()
+    {
+        return [
+            'pond_detail_id' => 'required|exists:pond_details,id',
+            'harvest_at' => 'required|date',
+            'weight' => 'required',
+            'image' => 'required|image'
+        ];
+    }
+}
