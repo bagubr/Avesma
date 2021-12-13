ed3f9977c2cc693716cd1dd6d2dd5ac97b1d049a
SATRIO <satriotol69@gmail.com>
2021-12-13 10:23:00 +0700

    test fixing

--- app/Http/Controllers/Api/ProcedureController.php
+++ app/Http/Controllers/Api/ProcedureController.php
@@ -17,7 +17,6 @@ use App\Models\Procedure;
 use App\Repositories\ProcedureRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
 
 class ProcedureController extends Controller
 {
@@ -50,11 +49,6 @@ class ProcedureController extends Controller
     }
     public function getFormProcedure($id)
     {
-        // $procedures_detail = FormProcedure::with('form_procedure_detail.form_procedure_detail_formulas')->where('fish_species_id', $request->fish_species_id)
-        //     ->when($request->procedure_id, function ($query) use ($request) {
-        //         $query->where('procedure_id', $request->procedure_id);
-        //     })->get()->first();
-
         $form_procedure = FormProcedure::where('id', $id)
             ->with('form_procedure_detail.form_procedure_detail_formulas')->first();
         $this->sendSuccessResponse([
@@ -71,18 +65,17 @@ class ProcedureController extends Controller
             'form_procedure_id' => $request->form_procedure_id,
         ]);
         foreach ($request->data as $i) {
-            dd($form_procedure_input_user->id);
             FormProcedureDetailInput::create([
                 'form_procedure_detail_id' => $i['form_procedure_detail_id'],
                 'form_procedure_detail_formula_id' => $i['form_procedure_detail_formula_id'],
-                'score' => FormProcedureDetailFormula::findOrFail($i['form_procedure_detail_formula_id'])->score,
                 'form_procedure_input_user_id' => $form_procedure_input_user->id,
+                'score' => FormProcedureDetailFormula::where('id', $i['form_procedure_detail_formula_id'])->first()->score,
             ]);
         };
         DB::commit();
 
         return $this->sendSuccessResponse([
-            'procedure' => $form_procedure_input_user
+            'procedure' => $form_procedure_input_user->load('form_procedure_detail_input')
         ]);
     }
     public function update(UpdateFormProcedureInputUserRequest $request, FormProcedureInputUser $form_procedure_input_user)
--- app/Models/FormProcedureInputUser.php
+++ app/Models/FormProcedureInputUser.php
@@ -39,8 +39,7 @@ class FormProcedureInputUser extends Model
 
     public function form_procedure_detail_input()
     {
-        return $this->hasMany(FormProcedureDetailInput::class, 'form_procedure_input_user_id', 'id')->where('min_range', '<=', $this->getTotalScoreAttribute())
-        ->where('max_range', '>=', $this->getTotalScoreAttribute());
+        return $this->hasMany(FormProcedureDetailInput::class, 'form_procedure_input_user_id', 'id');
     }
 
     public function getCreatedAtAttribute($value)
