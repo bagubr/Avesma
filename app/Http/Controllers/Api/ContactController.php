d55825e107510a988b8d73a4f6f196cfc133ce2e
SATRIO <satriotol69@gmail.com>
2022-01-04 09:52:25 +0700

    contact controller

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ContactController extends Controller
 {
     public function index()
     {
-        $contacts = Contact::all();
+        $contacts = Contact::where('type', '!=', 'WHATSAPP')->get();
+        $contact_regions = Contact::where('type', 'WHATSAPP')
+            ->where('region_id', Auth::user()->region_id)->get();
         return $this->sendSuccessResponse([
             'contacts' => $contacts,
+            'contact_regions' => $contact_regions,
         ]);
     }
 }
