7dfa52242ac7a1b7478bc3a5cb8dc4fdaf921ca1
Bagu bragowo <bagubragowo3@gmail.com>
2022-03-18 11:49:30 +0700

    fix bug

--- app/Http/Resources/ProcedureDetailResource.php
+++ app/Http/Resources/ProcedureDetailResource.php
@@ -17,7 +17,7 @@ class ProcedureDetailResource extends JsonResource
         return [
             "id" => $this->id,
             "form_procedure_detail_id" => $this->form_procedure_detail_id,
-            "form_procedure_detail_name" => $this->form_procedure_detail->name,
+            "form_procedure_detail_name" => $this->form_procedure_detail?->name??'',
             "form_procedure_detail_formula_id" => $this->form_procedure_detail_formula_id,
             "form_procedure_detail_formula_parameter" => $this->form_procedure_detail_formula->parameter,
             "score" => $this->score,
